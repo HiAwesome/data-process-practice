43ed883a064abd162cd24dc4c551b95b8322767c
moqi <moqimoqidea@gmail.com>
2020-02-16 14:38:31 +0800

    parse_excel.py 增加注释

--- code/chp04-excel/parse_excel.py
+++ code/chp04-excel/parse_excel.py
@@ -1,3 +1,10 @@
+# coding=utf-8
+"""
+    这是用来分析童工和童婚数据的脚本
+    本脚本中用到的 Excel 文件可以在以下链接中获取:
+        http://www.unicef.org/sowc2014/numbers/
+"""
+
 import pprint
 
 import xlrd
@@ -8,6 +15,7 @@ sheet = book.sheet_by_name('Table 9 ')
 
 data = {}
 
+# 从第 14 行开始，因为这是国家数据的起点
 for i in xrange(14, sheet.nrows):
     row = sheet.row_values(i)
 
@@ -24,6 +32,7 @@ for i in xrange(14, sheet.nrows):
         }
     }
 
+    # 最后一个国家是津巴布韦
     if country == 'Zimbabwe':
         break
 
